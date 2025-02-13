5f827b08477bc7b6d2045ee53185dfad628af244
Karan <50233829+hashira-kyojuro@users.noreply.github.com>
2025-02-13 21:18:37 +0530

    Add files via upload
    Latest DS Collection

---
+++ DataStructures/src/BinaryTree/Node.java
@@ -0,0 +1,13 @@
+package BinaryTree;
+
+public class Node {
+
+	int value;
+	Node left,right;
+	
+	Node(int value){
+		this.value=value;
+		this.left=this.right=null;
+	}
+	
+}
---
+++ DataStructures/src/DoublyLinkedList/Node.java
@@ -0,0 +1,16 @@
+package DoublyLinkedList;
+
+public class Node {
+
+	int data;
+	Node next;
+	Node prev;
+	
+	Node(int data){
+		this.data=data;
+		next=prev=null;
+		
+	}
+	
+	
+}
