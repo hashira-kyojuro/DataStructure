e6d477f5a3330e173b0210354f7be64193a36b97
Karan <50233829+hashira-kyojuro@users.noreply.github.com>
2025-04-09 16:21:02 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,6 +16,10 @@ public class App {
 	public static void changes(){
 		System.out.println("new change 2");
 	}
+
+	public static void newMethod(){
+		System.out.println("asdas");
+	}
 	
 	public static void addingThisMethodForNowTest(){
 		System.out.println("new change 2");
