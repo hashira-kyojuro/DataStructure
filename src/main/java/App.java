5bd8c52932783d6949b418a83001d4a24c3ef9eb
Karan <50233829+hashira-kyojuro@users.noreply.github.com>
2025-04-09 14:59:52 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,4 +5,9 @@ public class App {
 	public static void main(String[] args) {
 		System.out.println("Testing Connection new");
 	}
+
+
+	public static void newChange(){
+		System.out.println("This is it");
+	}
 }
